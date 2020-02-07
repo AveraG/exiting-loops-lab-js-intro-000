918be14f22762999f07fc08e4ef3ca6fad5a5c22
Avera Guthrie <averaguthrie@gmail.com>
2020-02-07 19:48:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,9 +23,9 @@ function keepGoing(array, changeValue, skipValue) {
 function findBy(array, findFn) {
   for(let i = 0; i < array.length; i++) {
     if(findFn(array[i])) {
-      return array[i];
-    } else {
       return null;
+    } else {
+      return array[i];
     }
   }
 }
